62305d2b3b77a5a972a9f53179dd298d84fcf723
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2023-05-04 15:51:47 +0300

    Fix test url

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,14 +1,16 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000');
+const baseUrl = 'http://localhost:8000';
 
+test.beforeEach(async ({ page }) => {
+  await page.goto(baseUrl);
+});
+
+test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Toppi/);
 });
 
 test('has työaikalaskuri', async ({ page }) => {
-  await page.goto('http://localhost:3000');
-
   await expect(page.getByRole('button')).toHaveText('Lähetä tarkistettavaksi')
 });
