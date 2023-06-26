5aa8418b6b9c559467b30d98c89a7b2ff18cfa8b
Veikkosuhonen <vesuvesuh@gmail.com>
2023-06-26 13:33:34 +0300

    Init users

---
+++ src/server/db/migrations/20230626_00_init_users.ts
@@ -0,0 +1,33 @@
+import { DataTypes } from 'sequelize'
+
+import { Migration } from '../connection'
+
+export const up: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.createTable("users", {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  })
+}
+
+export const down: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.dropTable("users")
+}
---
+++ src/server/db/models/User.ts
@@ -0,0 +1,58 @@
+import { CreationOptional, DataTypes, InferAttributes, InferCreationAttributes, Model } from "sequelize";
+import { sequelize } from '../connection'
+
+
+class User extends Model<
+  InferAttributes<User>,
+  InferCreationAttributes<User>
+> {
+  declare id: CreationOptional<string>
+
+  declare firstName: string;
+
+  declare lastName: string;
+
+  declare email: string;
+
+  declare createdAt: CreationOptional<Date>;
+
+  declare updatedAt: CreationOptional<Date>;
+
+  toPublic() {
+    return {
+      id: this.dataValues.id,
+      createdAt: this.dataValues.createdAt,
+      updatedAt: this.dataValues.updatedAt,
+    }
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+    }
+  },
+  {
+    underscored: true,
+    sequelize,
+  }
+)
+
+export default User
