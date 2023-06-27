7912e17d7abd706318a389dc2909127a55421c24
Veikko Suhonen <veikko.suhonen@helsinki.fi>
2023-06-27 10:01:54 +0300

    Fix tests

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -30,11 +30,11 @@ test('Fill required contract request form', async ({ page }) => {
   await page.getByLabel('Etunimi *').fill('Veikko');
   await page.getByLabel('Sukunimi *').fill('Suhonen');
   await page.getByLabel('Syntymäaika *').fill('1984-02-03');
-  await page.getByPlaceholder('Anna sähköpostisi').fill('veijo@gmail.com');
-  await page.getByPlaceholder('Anna kurssin nimi').fill('Dota 2 perusteet');
+  await page.getByLabel('Sähköposti').fill('veijo@gmail.com');
+  await page.getByLabel('Kurssin nimi').fill('Dota 2 perusteet');
   await page.getByLabel('Ensimmäinen luento *').fill('2023-05-27');
   await page.getByLabel('Viimeinen luento/tentti *').fill('2023-05-19');
-  await page.getByRole('button', { name: 'Lähetä tarkistettavaksi' }).click();
+  await page.getByRole('button', { name: 'Lähetä käsiteltäväksi' }).click();
   await page.getByText('Lähetetään työsopimuspyyntöä').click();
 });
 
@@ -42,13 +42,13 @@ test('Fill full contract request form', async ({ page }) => {
   await page.getByLabel('Etunimi *').fill('Pekka');
   await page.getByLabel('Sukunimi *').fill('Perhonen');
   await page.getByLabel('Syntymäaika *').fill('1984-03-02');
-  await page.getByPlaceholder('Anna sähköpostisi').fill('peggoperhonen@buttermail.com');
-  await page.getByPlaceholder('Anna kurssin nimi').fill('Lentämisen perusteet');
+  await page.getByLabel('Sähköposti').fill('peggoperhonen@buttermail.com');
+  await page.getByLabel('Kurssin nimi').fill('Lentämisen perusteet');
   await page.getByLabel('Ensimmäinen luento *').fill('2024-01-08');
   await page.getByLabel('Viimeinen luento/tentti *').fill('2024-01-10');
   await page.getByLabel('Muu aikaväli').check();
   await page.getByLabel('Valitse alkupäivä *').fill('2023-06-01');
   await page.getByLabel('Valitse loppupäivä *').fill('2023-06-30');
-  await page.getByRole('button', { name: 'Lähetä tarkistettavaksi' }).click();
+  await page.getByRole('button', { name: 'Lähetä käsiteltäväksi' }).click();
   await page.getByText('Lähetetään työsopimuspyyntöä').click();
 })
