baf6b31c3689e896f6bfa53fa1570e4813a061c6
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-08-22 09:29:19 +0300

    Switch to new Sentry

--- src/client/util/sentry.ts
+++ src/client/util/sentry.ts
@@ -4,7 +4,7 @@ import { GIT_SHA } from '../../config'
 
 const initializeSentry = () => {
   Sentry.init({
-    dsn: 'https://06393e6c223e4dc69d476c94e74dee75@sentry.cs.helsinki.fi/16',
+    dsn: 'https://40576de3744c94303a0c2cbfc3ff0a21@toska.cs.helsinki.fi/4',
     release: GIT_SHA,
     integrations: [new Integrations.BrowserTracing()],
     tracesSampleRate: 1.0,
--- src/server/middleware/sentry.ts
+++ src/server/middleware/sentry.ts
@@ -3,7 +3,7 @@ import { ErrorRequestHandler, RequestHandler, Router } from 'express'
 
 export const initSentry = (app: Router) =>
   Sentry.init({
-    dsn: 'https://06393e6c223e4dc69d476c94e74dee75@sentry.cs.helsinki.fi/16',
+    dsn: 'https://40576de3744c94303a0c2cbfc3ff0a21@toska.cs.helsinki.fi/4',
     integrations: [new Sentry.Integrations.Http({ tracing: true }), new Sentry.Integrations.Express({ app })],
     tracesSampleRate: 1.0,
   })
