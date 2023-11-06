c71c5d13ed485e68bdba481392c9d1fa3af34f2f
Veikkosuhonen <vesuvesuh@gmail.com>
2023-11-06 13:25:29 +0200

    fix: Dont throw when login gets 401, meaningless

--- src/client/hooks/useCurrentUser.ts
+++ src/client/hooks/useCurrentUser.ts
@@ -1,11 +1,17 @@
 import { UserParams } from '../../shared/types'
 import { useQuery } from '@tanstack/react-query'
 import { login } from '../api'
+import { AxiosError } from 'axios'
 
 export const useCurrentUser = () => {
   const { data: user, ...rest } = useQuery<UserParams>({
     queryKey: ['currentUser'],
     queryFn: login,
+    retry: false,
+    throwOnError: (error) => {
+      if (error instanceof AxiosError && error.response?.status === 401) return false
+      return true
+    },
   })
 
   return { user, ...rest }
